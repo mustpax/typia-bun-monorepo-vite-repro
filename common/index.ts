cbdbec283c06fba2799f7f6e571bf1fe7f70c84e
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2025-02-19 09:58:12 -0800

    wip

--- common/index.ts
+++ common/index.ts
@@ -12,3 +12,26 @@ export interface AppState {
     resetConfirmPending: boolean;
   };
 }
+
+export const exampleAppState: AppState = {
+  tasks: [
+    {
+      id: "task_1",
+      name: "Task 1",
+      completed: true,
+      time: "00:10",
+    },
+    {
+      id: "task_2",
+      name: "Task 2",
+      completed: false,
+      time: null,
+    },
+  ],
+  timerState: {
+    startedAt: null,
+    totalElapsed: 0,
+    isRunning: false,
+    resetConfirmPending: false,
+  },
+};
