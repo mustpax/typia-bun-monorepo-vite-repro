6fbc27d50d39aa80cf59ee63a9f32dd9a35f4bf3
Mustafa "Moose" Paksoy <paksoy@gmail.com>
2025-02-20 13:50:48 -0800

    wip

--- common/index.ts
+++ common/index.ts
@@ -1,37 +1,10 @@
 export interface AppState {
-  tasks: Array<{
-    id: string;
-    name: string;
-    completed: boolean;
-    time: string | null;
-  }>;
-  timerState: {
-    startedAt: number | null;
-    totalElapsed: number;
-    isRunning: boolean;
-    resetConfirmPending: boolean;
-  };
+  tasks: { id: string; name: string | null }[];
 }
 
 export const exampleAppState: AppState = {
   tasks: [
-    {
-      id: "task_1",
-      name: "Task 1",
-      completed: true,
-      time: "00:10",
-    },
-    {
-      id: "task_2",
-      name: "Task 2",
-      completed: false,
-      time: null,
-    },
+    { id: "task_1", name: null },
+    { id: "task_2", name: "Task 2" },
   ],
-  timerState: {
-    startedAt: null,
-    totalElapsed: 0,
-    isRunning: false,
-    resetConfirmPending: false,
-  },
 };
